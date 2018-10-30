cc059346284bdd4f260e311e0b15a520a07d9078
Uche Jude <iamuchejude@gmail.com>
2018-10-30 22:28:40 +0100

    feat(login): add login feature
    - create login component
    - creats actions and reducers for login feature
    
    [Finishes #161583789]

---
+++ src/actions/login.js
@@ -0,0 +1,50 @@
+import {
+  LOGIN_FAILED,
+  LOGIN_SUCCESS,
+  LOGIN_REQUEST,
+} from './types/auth';
+
+const API_HOST = 'http://localhost:8888/api/v1'
+
+export const loginFailed = message => ({
+  type: LOGIN_FAILED,
+  payload: message,
+});
+
+export const loginSuccess = message => ({
+  type: LOGIN_SUCCESS,
+  payload: message,
+});
+
+export const loginRequest = state => ({
+  type: LOGIN_REQUEST,
+  payload: state,
+});
+
+const login = data => dispatch => {
+  dispatch(loginRequest(true));
+  return fetch(`${API_HOST}/auth/login`, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
+    .then(res => res.json())
+    .then((data) => {
+      if (data.status !== 'success') {
+        dispatch(loginFailed(data.message));
+        return dispatch(loginRequest(false));
+      }
+      dispatch(loginSuccess(data.message));
+      return dispatch(loginRequest(false));
+    })
+    .catch(error => {
+      if (error) {
+        dispatch(loginFailed('Error occured. Please try again later'));
+      }
+      return dispatch(loginRequest(false));
+    });
+}
+
+export default login;
